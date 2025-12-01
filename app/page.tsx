99c0151dad986b18317a7b3dc4f0a30a753ec824
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2025-12-01 07:26:02 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import HeroSlider from "../components/HeroSlider";
+import Highlights from "../components/Highlights";
+import Experiences from "../components/Experiences";
+import BinationalRoutes from "../components/BinationalRoutes";
+import WhyChooseUs from "../components/WhyChooseUs";
+import Testimonials from "../components/Testimonials";
+import FAQ from "../components/FAQ";
+import ContactSection from "../components/ContactSection";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSlider />
+      <Highlights />
+      <Experiences />
+      <BinationalRoutes />
+      <WhyChooseUs />
+      <Testimonials />
+      <FAQ />
+      <ContactSection />
+    </>
+  );
+}
